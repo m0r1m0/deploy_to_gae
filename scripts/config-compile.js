55bda98c9fd0edf3e24868adc32d8815cc11b8b4
pywd <yuki.wd7@gmail.com>
2020-07-27 00:03:15 +0900

    first commit

---
+++ scripts/config-compile.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+const template = fs.readFileSync('app.template.yaml').toString();
+let content = template;
+
+for (const match of template.matchAll(/\${{([A-Z_]*)}}/g)) {
+  const envName = `APP_${match[1]}`;
+  content = content.replace(match[0], `'${process.env[envName]}'`);
+}
+
+fs.writeFileSync('app.yaml', content);
